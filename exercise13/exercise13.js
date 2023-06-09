654d235709ec4566850f85b4f5c7ccc0ce16a687
Ana Beatriz Gonçalves <E1476@itsector.pt>
2023-06-09 16:28:11 +0100

    Exercise 13.

---
+++ exercise13/exercise13.js
@@ -0,0 +1,48 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let randomText = "";
+
+const generateRandomText = () => {
+    randomText = "";
+    const charactersLength = characters.length;
+    for (let i = 0; i < charactersLength; i++) {
+        randomText += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    updateRandomText(randomText);
+}
+
+const updateRandomText = (randomText) => {
+    let text = document.getElementById("randomText");
+    text.innerHTML = randomText;
+}
+
+const submitInformation = () => {
+    let text = document.getElementById("text").value;
+    let position = document.getElementById("position").value;
+
+    let result = verifyText(randomText, text, position);
+    updateInformation(result);
+}
+
+const verifyText = (randomText, text, position) => {
+    return result = randomText.test(text, position);
+}
+
+const updateInformation = (result) => {
+    let textResult = document.getElementById("result");
+    textResult.innerHTML = `The result is: ${result}`;
+}
+
+String.prototype.test = function (textToCompare, startIndex = 0) {
+    let charactersToIndexInserted = this.toString().substring(startIndex);
+
+    let textToCompareLength = textToCompare.length;
+
+    let numOfCharToSlice = charactersToIndexInserted.length - textToCompareLength;
+
+    let indexToRemove = charactersToIndexInserted.length - numOfCharToSlice;
+
+    let originalStringToCompare = charactersToIndexInserted.slice(0, indexToRemove)
+
+    return originalStringToCompare === textToCompare;
+};
+
